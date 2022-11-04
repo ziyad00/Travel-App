08aaa48edad885b7b654135a92950612503bd722
ziyad00 <ziyad.alotaibe@gmail.com>
2022-11-04 20:41:40 +0300

    add place card

--- lib/widgets/choose_place.dart
+++ lib/widgets/choose_place.dart
@@ -14,8 +14,13 @@ class ChoosePlace extends StatelessWidget {
         scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.all(8),
         children: [
-          PlaceCard(),
-          PlaceCard(),
+          PlaceCard(imagePath: 'images/4.png'),
+          SizedBox(
+            width: 20,
+          ),
+          PlaceCard(
+            imagePath: 'images/6.png',
+          ),
         ],
       ),
     );
@@ -23,8 +28,10 @@ class ChoosePlace extends StatelessWidget {
 }
 
 class PlaceCard extends StatelessWidget {
+  final String imagePath;
   const PlaceCard({
     Key? key,
+    required this.imagePath,
   }) : super(key: key);
 
   @override
@@ -32,11 +39,13 @@ class PlaceCard extends StatelessWidget {
     return Container(
         width: 186,
         height: 186,
-        padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
+        padding: EdgeInsets.fromLTRB(10, 10, 15, 0),
         decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(30.0)),
             image: DecorationImage(
-          image: AssetImage('images/4.png'),
-        )), //s
+              image: AssetImage('$imagePath'),
+              fit: BoxFit.fill,
+            )), //s
         child: Column(
           children: [
             Container(
@@ -44,22 +53,95 @@ class PlaceCard extends StatelessWidget {
               child: Container(
                 width: 32,
                 height: 32,
-                padding: EdgeInsets.fromLTRB(3, 0, 0, 0),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.all(Radius.circular(15.0)),
                     color: Colors.white),
-                alignment: Alignment.centerRight,
-                child: Text(
-                  "ssss",
-                ),
+                alignment: Alignment.center,
+                child: Image.asset('images/heart.png'),
               ),
             ),
-            Text(
-              "Mountain",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
+            SizedBox(
+              height: 100,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 50),
+              child: Column(
+                children: [
+                  Text(
+                    "Kuta Beach",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('images/location.png'),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "Bali, Indonesia",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                        size: 12,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                        size: 12,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow,
+                        size: 12,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.white,
+                        size: 12,
+                      ),
+                      Icon(
+                        Icons.star,
+                        color: Colors.white,
+                        size: 12,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "4.2",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ));
